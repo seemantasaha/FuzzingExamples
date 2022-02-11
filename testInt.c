4b3f7706d06736fcaec7fdc9e780f91c289141bf
Seemanta Saha <seemantasaha@gmail.com>
2022-02-10 19:16:16 -0800

    Adding a test example for integer value fuzzing

---
+++ testInt.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void fuzz(int i) {
+    if (i > 0) {
+        printf("Positive input");
+        if (i > 1073741823) {
+            printf("Greater than 2 to the power 30");
+            if (i > 65535) {
+                printf("Greater than 2 to the power 16");
+                if (i > 256) {
+                    printf("Greater than 2 to the power 8");
+                    if (i > 16) {
+                        printf("Greater than 2 to the power 4");
+                        if (i > 8) {
+                            printf("Greater than 2 to the power 3");
+                            if (i > 4) {
+                                printf("Greater than 2 to the power 2");
+                                if (i > 1) {
+                                    printf("Greater than 2 to the power 1");
+                                } else {
+                                    printf("Less than 2 to the power 1");
+                                }
+                            } else {
+                                printf("Less than 2 to the power 2");
+                            }
+                        } else {
+                            printf("Less than 2 to the power 3");
+                        }
+                    } else {
+                        printf("Less than 2 to the power 4");
+                    }
+                } else {
+                    printf("Less than 2 to the power 8");
+                }
+            } else {
+                printf("Less than 2 to the power 16");
+            }
+        } else {
+            printf("Less than 2 to the power 30");
+        }
+        
+    } else {
+        printf("Negative input");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int input = atoi(argv[0]);
+    printf("input = %d", input);
+    fuzz(input);
+
+    return 0;
+}
